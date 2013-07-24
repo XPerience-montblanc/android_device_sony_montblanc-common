db6b68255267ed17da7aac2d0f8ab9ce0760c958
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2013-07-24 17:15:14 -0500

    mMAx hiden apps

--- montblanc.mk
+++ montblanc.mk
@@ -98,7 +98,7 @@ PRODUCT_COPY_FILES += \
     device/sony/montblanc-common/config/cacert.txt:system/etc/suplcert/cacert.txt
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    sys.mem.max_hidden_apps=10
+    sys.mem.max_hidden_apps=15
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp \
